1511f29c3f227bee8aadc5d2c7fc4543110b1ae2
Myronious <myroniousk@yahoo.com>
2022-06-14 19:06:29 -0400

    added some links

--- minting-dapp/src/scripts/react/Dapp.tsx
+++ minting-dapp/src/scripts/react/Dapp.tsx
@@ -65,7 +65,7 @@ export default class Dapp extends React.Component<Props, State> {
     if (browserProvider?.isMetaMask !== true) {
       this.setError( 
         <>
-          <hr /><br />This minter USES POLYGON NATIVE CHAIN MATIC TOKENS **ONLY**  <br /><br />Prices on this minter are in MATIC NATIVE *NOT MATIC ERC-20* <br /><br />IF YOU ARE USING ETHEREUM NATIVE MATIC, PLEASE BUY FROM OUR OPENSEA STORE<br /><br />
+          <hr /><br />This minter USES POLYGON NATIVE CHAIN MATIC TOKENS **ONLY**  <br /><br /><br />Prices on this minter are in MATIC NATIVE *NOT MATIC ERC-20*<br />IF YOU ARE USING ETHEREUM NATIVE MATIC, PLEASE BUY FROM OUR OPENSEA STORE<br /><br />
           <br /><br /><strong>ON A DESKTOP? Use <a href="https://www.google.com/chrome/" target="_blank">CHROME BROWSER</a> with the <a href="https://metamask.io" target="_blank">MetaMask</a>or <a href="https://www.coinbase.com/wallet/getting-started-extension" target="_blank">Coinbase Wallet</a> extensions.</strong>
           <br /><br /> <br /> 
          <strong>ON A MOBILE DEVICE? <br />Use the Metamask or Coinbase links below to connect through your wallet app.</strong> <br /><br />
@@ -211,13 +211,13 @@ export default class Dapp extends React.Component<Props, State> {
             {!this.isWalletConnected() ? <button className="primary" disabled={this.provider === undefined} onClick={() => this.connectWallet()}>Connect Wallet</button> : null}
             
             <div className="use-block-explorer">
-              <hr /><br />This minter USES POLYGON NATIVE CHAIN MATIC TOKENS **ONLY**.  <br />Prices on this minter are in MATIC NATIVE (NOT ERC-20). <br />IF YOU ARE USING ETHEREUM NATIVE MATIC, PLEASE BUY FROM OUR OPENSEA STORE<br /><br />
-              <hr /><br />Each NFT is a 1-of-1 artwork, based on the JD MOON logo. <br />Traits and rarities are SECRET until we REVEAL them. :)<br /><br />
-              <hr /><br />THERE ARE *ONLY* 420 FIRST-EDITION MINTS. <br />Early holders will get perks airdropped for their support. BIG THANKS to you.<br /><br />
+              <hr /><br />This minter USES POLYGON NATIVE MATIC TOKENS **ONLY**.  <br /><br /> <a href="https://chainlist.org/chain/137" target="_blank">Add POLYGON to MetaMask through this link.</a> <br /><br />IF YOU ARE USING ETHEREUM NATIVE MATIC, PLEASE BUY FROM OUR OPENSEA STORE<br /><br />
+              <hr /><br />Each JD MOON NFT is a 1-of-1 artwork <br /><br />Traits and rarities are SECRET until we REVEAL them. :)<br /><br />
+              <hr /><br />THERE ARE *ONLY* 420 FIRST-EDITION MINTS. <br /><br />Early holders will get perks airdropped for their support. BIG THANKS to you.<br /><br />
               <hr /><br />PHASE 1: WHITELIST - Limit of 10 NFTs per transaction. Price per NFT = 55 MATIC + gas. <br /><br />
               <hr /><br />PHASE 2: PRESALE - Limit of 15 NFTs per transaction. Price per NFT = 85 MATIC + gas. <br /><br />  
               <hr /><br />PHASE 3: PUBLIC SALE - Limit of 50 NFTs per transaction. Price per NFT = 110 MATIC + gas<br /><br />  
-              <hr /><br />PHASE 4: The collection's rarities will be REVEALED. <br />Mint limit is 50. <br /><br /> 
+              <hr /><br />PHASE 4: The collection's rarities will be REVEALED. <br /><br />Mint limit is 50. <br /><br /> 
               <strong>Thank you for supporting our move into web 3! The future is bright, it's the wild west, the sky is the limit. </strong> <span className="emoji">🎶</span><br /><br />
               <hr /><br />You can interact with the smart-contract <strong>directly</strong> through <a href={this.generateContractUrl()} target="_blank">{this.state.networkConfig.blockExplorer.name}</a>, without even connecting your wallet to this DAPP! <span className="emoji">🚀</span><br /><br />
               Thank you! <span className="emoji">🎶</span><br /><br />
